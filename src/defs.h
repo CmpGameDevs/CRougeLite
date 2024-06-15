7f7fac5ed63446c137167968f295f27343d3b6ab
amir-kedis <amir.kedis@gmail.com>
2024-06-15 12:16:43 +0300

    shooter up 3

--- src/defs.h
+++ src/defs.h
@@ -4,5 +4,7 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
+#define PLAYER_SPEED 5
+
 
 #endif // DEFS_H
--- src/engine/input.c
+++ src/engine/input.c
@@ -1,8 +1,20 @@
 #include "input.h"
-#include <raylib.h>
+#include "../CRougeLite.h"
 
 void handleInput() {
-  if (IsKeyPressed(KEY_ESCAPE) || IsKeyPressed(KEY_Q)) {
+  if (IsKeyPressed(KEY_ESCAPE)) {
     CloseWindow();
+  }
+  if (IsKeyDown(KEY_W)) {
+    player.y -= PLAYER_SPEED;
+  }
+  if (IsKeyDown(KEY_S)) {
+    player.y += PLAYER_SPEED;
+  }
+  if (IsKeyDown(KEY_A)) {
+    player.x -= PLAYER_SPEED;
+  }
+  if (IsKeyDown(KEY_D)) {
+    player.x += PLAYER_SPEED;
   }
 }
