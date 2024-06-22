763d5af8e118985137762bfa8a264722c014bfaa
amir-kedis <amir.kedis@gmail.com>
2024-06-22 17:09:11 +0300

    feat(animation-system): new Animator System ready to go

--- src/system/anime.c
+++ src/system/anime.c
@@ -30,3 +30,55 @@ Rectangle getSrcRect(Animator *animator) {
   SpriteAnimation *anim = &animator->animatinos[animator->currentState];
   return getAtlasImage(anim->frameNames[anim->currentFrame]).source;
 }
+
+void setState(Animator *animator, int state) {
+  if (animator->currentState == state)
+    return;
+
+  animator->currentState = state;
+  animator->animatinos[state].currentFrame = 0;
+  animator->animatinos[state].frameCount = 0;
+  animator->animatinos[state].isFinished = false;
+  animator->isFinished = false;
+}
+
+void updateAnimator(Animator *animator) {
+  SpriteAnimation *anim = &animator->animatinos[animator->currentState];
+
+  if (anim->isFinished && !anim->isLooping) {
+    animator->isFinished = true;
+    return;
+  }
+
+  if (anim->isLooping)
+    animator->isFinished = true;
+  else
+    animator->isFinished = false;
+
+  anim->frameCount++;
+  double divisor = 60.0f / anim->fps;
+  anim->currentFrame = anim->frameCount / divisor;
+
+  if (anim->currentFrame >= anim->numOfFrames) {
+    anim->currentFrame = 0;
+    anim->frameCount = 0;
+    anim->isFinished = true;
+  }
+
+  if (anim->isFinished && anim->isLooping) {
+    anim->currentFrame = 0;
+    anim->frameCount = 0;
+    anim->isFinished = false;
+  }
+}
+
+void drawAnimator(Animator *animator, CTransform *transform, Color tint,
+                  bool flipX) {
+  SpriteAnimation *anim = &animator->animatinos[animator->currentState];
+  Rectangle src = getSrcRect(animator);
+  DrawAtlasSpritePro(anim->frameNames[anim->currentFrame],
+                     (Rectangle){transform->position.x, transform->position.y,
+                                 src.width * transform->scale.x,
+                                 src.height * transform->scale.y},
+                     (Vector2){0, 0}, 0, tint, flipX);
+}
--- src/system/anime.h
+++ src/system/anime.h
@@ -32,5 +32,37 @@
  */
 Rectangle getSrcRect(Animator *animator);
 
+/* setState
+ *
+ * Set the state of the animator to the specified state.
+ *
+ * @param animator - The animator to set the state of.
+ * @param state - The state to set the animator to.
+ *
+ */
+void setState(Animator *animator, int state);
+
+/* updateAnimator
+ *
+ * Update the animator to the next frame of the animation.
+ *
+ * @param animator - The animator to update.
+ *
+ */
+void updateAnimator(Animator *animator);
+
+/* drawAnimator
+ *
+ * Draw the animator to the screen.
+ *
+ * @param animator - The animator to draw.
+ * @param transform - The transform of the animator.
+ * @param tint - The tint color of the animator.
+ * @param flipX - Whether to flip the animator on the x-axis.
+ *
+ */
+void drawAnimator(Animator *animator, CTransform *transform, Color tint,
+                  bool flipX);
+
 #endif // ANIME_H
 // vim: fdm=marker
