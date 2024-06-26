eb8c75dca97dd62df4c63ec57bc700f890950e08
Mo2Hefny <moamensaied1@gmail.com>
2024-06-26 21:39:42 +0300

    refactor: refactor player keyboard input system

--- src/game/player.h
+++ src/game/player.h
@@ -24,9 +24,24 @@
 #include "../CRougeLite.h"
 #include "weapon.h"
 
+/**
+ * setupPlayers - initialize all enemies manually to the game state
+ */
 void setupPlayers();
+
+/**
+ * drawPlayers - draw players on the scene
+ */
 void drawPlayers();
+
+/**
+ * updatePlayers - update the players objects every game tick
+ */
 void updatePlayers();
+
+/**
+ * clearPlayers - free players array from heap
+ */
 void clearPlayers();
 
 #endif // PLAYER_H
